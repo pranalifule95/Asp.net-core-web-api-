82cb5c4382f418de2de7facd98a1a6c579b6ac0c
Pranali <pranali.fule@after.co>
2023-10-09 23:55:48 +0530

    This is the first commit of our repository

---
+++ my-book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace my_book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
